6b6c9f4d98e25c9b156474c9bd086d43c692d8df
xuebaba1115 <465258148@qq.com>
2018-04-11 15:34:52 +0800

    #

--- echo_wsgi/server.py
+++ echo_wsgi/server.py
@@ -122,6 +122,12 @@ def new_user():
     db.session.commit()
     return jsonify({ 'username': user.username })
 
+@app.route('/api/users/<int:id>')
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username})
 
 @app.route('/api/resource')
 @auth.login_required
