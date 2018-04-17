366260e9183fda3508672673566db7707ad6f292
xuebaba1115 <465258148@qq.com>
2018-04-17 19:37:42 +0800

    #ok

--- echo_wsgi/run.py
+++ echo_wsgi/run.py
@@ -91,21 +91,23 @@ def get_auth_token():
 
 @app.route('/api/wxauth')
 def wxauth():
-    # try:
-    #     sessionKey=request.headers['constants.WX_HEADER_CODE']
-    #     encryptedData=request.headers['constants.WX_HEADER_ENCRYPTED_DATA']
-    #     iv=request.headers['constants.WX_HEADER_IV']
-    # except Exception as e:
-    #     log.msg(str(e))
-    #     abort(400)
-    appId = 'wx4f4bc4dec97d474b'
-    sessionKey = 'tiihtNczf5v6AKRyjwEUhQ=='
-    encryptedData = 'CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZMQmRzooG2xrDcvSnxIMXFufNstNGTyaGS9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+3hVbJSRgv+4lGOETKUQz6OYStslQ142dNCuabNPGBzlooOmB231qMM85d2/fV6ChevvXvQP8Hkue1poOFtnEtpyxVLW1zAo6/1Xx1COxFvrc2d7UL/lmHInNlxuacJXwu0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn/Hz7saL8xz+W//FRAUid1OksQaQx4CMs8LOddcQhULW4ucetDf96JcR3g0gfRK4PC7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns/8wR2SiRS7MNACwTyrGvt9ts8p12PKFdlqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYVoKlaRv85IfVunYzO0IKXsyl7JCUjCpoG20f0a04COwfneQAGGwd5oa+T8yO5hzuyDb/XcxxmK01EpqOyuxINew=='
-    iv = 'r7BXXKkLb8qrSNn05n0qiA=='
+    try:
+        qcode=request.headers['constants.WX_HEADER_CODE']
+        encryptedData=request.headers['constants.WX_HEADER_ENCRYPTED_DATA']
+        iv=request.headers['constants.WX_HEADER_IV']
+    except Exception as e:
+        log.msg(str(e))
+        abort(400)
 
-    pc = WXBizDataCrypt(appId, sessionKey)
+    print iv,sessionKey,qcode
+    # appId = 'wx4f4bc4dec97d474b'
+    # sessionKey = 'tiihtNczf5v6AKRyjwEUhQ=='
+    # encryptedData = 'CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZMQmRzooG2xrDcvSnxIMXFufNstNGTyaGS9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+3hVbJSRgv+4lGOETKUQz6OYStslQ142dNCuabNPGBzlooOmB231qMM85d2/fV6ChevvXvQP8Hkue1poOFtnEtpyxVLW1zAo6/1Xx1COxFvrc2d7UL/lmHInNlxuacJXwu0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn/Hz7saL8xz+W//FRAUid1OksQaQx4CMs8LOddcQhULW4ucetDf96JcR3g0gfRK4PC7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns/8wR2SiRS7MNACwTyrGvt9ts8p12PKFdlqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYVoKlaRv85IfVunYzO0IKXsyl7JCUjCpoG20f0a04COwfneQAGGwd5oa+T8yO5hzuyDb/XcxxmK01EpqOyuxINew=='
+    # iv = 'r7BXXKkLb8qrSNn05n0qiA=='
 
-    print pc.decrypt(encryptedData, iv)
+    # pc = WXBizDataCrypt(appId, sessionKey)
+
+    # print pc.decrypt(encryptedData, iv)
     return "ssa"
 
 
