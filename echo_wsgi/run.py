c4faae5742d9cb49a4c6ea6ad3872c700466837f
xuebaba1115 <465258148@qq.com>
2018-04-17 20:32:45 +0800

    #add requests

--- echo_wsgi/run.py
+++ echo_wsgi/run.py
@@ -1,5 +1,6 @@
 import uuid
 import sys,os
+import requests
 
 from utils import WXBizDataCrypt
 
@@ -27,6 +28,7 @@ app.secret_key = str(uuid.uuid4())
 
 
 #config
+app.config.from_object('config.py')
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
@@ -92,7 +94,6 @@ def get_auth_token():
 @app.route('/api/wxauth')
 def wxauth():
     try:
-        print request.headers
         qcode=request.headers['X-WX-Code']
         encryptedData=request.headers['X-WX-Encrypted-Data']
         iv=request.headers['X-WX-IV']
@@ -100,15 +101,18 @@ def wxauth():
         log.msg(str(e))
         abort(400)
 
-    print iv,qcode
-    # appId = 'wx4f4bc4dec97d474b'
-    # sessionKey = 'tiihtNczf5v6AKRyjwEUhQ=='
-    # encryptedData = 'CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZMQmRzooG2xrDcvSnxIMXFufNstNGTyaGS9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+3hVbJSRgv+4lGOETKUQz6OYStslQ142dNCuabNPGBzlooOmB231qMM85d2/fV6ChevvXvQP8Hkue1poOFtnEtpyxVLW1zAo6/1Xx1COxFvrc2d7UL/lmHInNlxuacJXwu0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn/Hz7saL8xz+W//FRAUid1OksQaQx4CMs8LOddcQhULW4ucetDf96JcR3g0gfRK4PC7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns/8wR2SiRS7MNACwTyrGvt9ts8p12PKFdlqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYVoKlaRv85IfVunYzO0IKXsyl7JCUjCpoG20f0a04COwfneQAGGwd5oa+T8yO5hzuyDb/XcxxmK01EpqOyuxINew=='
-    # iv = 'r7BXXKkLb8qrSNn05n0qiA=='
 
-    # pc = WXBizDataCrypt(appId, sessionKey)
+# https://api.weixin.qq.com/sns/jscode2session?appid=wx4a6f0f1d44f64ca8&secret=09039f0ae70aca0a3096afc06c7a8872&js_code=071yR05z06Ctqf1TkK4z0p345z0yR05B&grant_type=authorization_code
+    appId = app.config['WX-APPID']
+    secret= app.config['WX-SECRET']
+    r=requests.get('https://api.weixin.qq.com/sns/jscode2session?appid=%&secret=%s&js_code=%s&grant_type=authorization_code'%(appId,secret,qcode))
 
-    # print pc.decrypt(encryptedData, iv)
+    sessionKey = 'tiihtNczf5v6AKRyjwEUhQ=='
+ 
+
+    pc = WXBizDataCrypt(appId, sessionKey)
+    print pc.decrypt(encryptedData, iv)
+
     return "ssa"
 
 
