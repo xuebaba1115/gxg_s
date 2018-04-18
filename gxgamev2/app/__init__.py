c5f422405d7478bb56b0dadc8bcc1606a569513f
xuebaba1115 <465258148@qq.com>
2018-04-18 18:28:38 +0800

    #ok lujing abs

--- gxgamev2/app/__init__.py
+++ gxgamev2/app/__init__.py
@@ -4,11 +4,12 @@ import uuid
 import sys
 import os
 
+abs=os.path.abspath('.')
 
 app = Flask(__name__)
 app.config.from_object('app.config')
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////%s/db.sqlite'%(abs)
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_ECHO'] = True
