635f9eb240f66ec964b80ceb631630b8ba1442ea
xuebaba1115 <465258148@qq.com>
2018-04-30 01:01:56 +0800

    #ok init

--- gxgamev2/app/game_A.py
+++ gxgamev2/app/game_A.py
@@ -4,7 +4,7 @@ from twisted.internet import reactor
 import json
 import time
 import random
-from twisted.internet.defer import Deferred, DeferredList, gatherResults,returnValue
+from twisted.internet.defer import Deferred, DeferredList, gatherResults, returnValue
 
 
 class Gamemanger(object):
@@ -19,29 +19,29 @@ class Gamemanger(object):
         except Exception as e:
             log.msg(str(e))
 
-    def register(self, json_data, connid):                           
+    def register(self, json_data, connid):
         print "register"
         _ps = pople(json_data["name"], connid,
-                    json_data["x"], json_data["y"])
+                    json_data["x"], json_data["y"], json_data["playerType"])
         if _ps.connid in self.clients:
             raise Exception("System record conflict")
         self.clients[_ps.connid] = _ps
         pl, players = self.getallpopleinfo("init")
-        return pl,players
+        _ps.playerType = 2
+        return pl, players
 
-    def getallpopleinfo(self,command):
+    def getallpopleinfo(self, command):
         _data = []
-        _cid = []
         for p in self.clients.values():
             _data.append({"connid": p.connid, 'team': 0, 'tankType': 1, 'playerID': p.connid,
-                          'playerType': 1, "pos": {"x": p.x, "y": p.y}, "name": p.name})
-            _cid.append(p.connid)
-        return _cid, {"command": command, "players": _data}
+                          "playerType": p.playerType, "pos": {"x": p.x, "y": p.y}, "name": p.name})
+        return self.clients.keys(), {"command": command, "players": _data}
 
     def actions(self, json_data):
-        p = self.clients.get(json_data['player']['connid'],None)
-        _data =  {"connid": p.connid, 'team': 0, 'tankType': 1, 'playerID': p.connid,'playerType': 1, "pos": {"x": p.x, "y": p.y}, "name": p.name}
-        return self.clients.keys(),{"command": json_data['command'], "player": _data}
+        p = self.clients.get(json_data['player']['connid'], None)
+        _data = {"connid": p.connid, 'team': 0, 'tankType': 1, 'playerID': p.connid,
+                 'playerType': 1, "pos": {"x": p.x, "y": p.y}, "name": p.name}
+        return self.clients.keys(), {"command": json_data['command'], "player": _data}
 
     def handledata(self, json_data):
         d = Deferred()
@@ -54,8 +54,9 @@ class Gamemanger(object):
 
 
 class pople(object):
-    def __init__(self, name, connid, x, y):
+    def __init__(self, name, connid, x, y, playerType):
         self.connid = connid
         self.name = name
         self.x = x
         self.y = y
+        self.playerType = playerType
