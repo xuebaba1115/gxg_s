aca6645817a3924cdde5f21a9de4be8cf7a98b9b
xuebaba1115 <465258148@qq.com>
2018-05-01 12:55:26 +0800

    #ok rotation

--- gxgamev2/app/game_A.py
+++ gxgamev2/app/game_A.py
@@ -21,57 +21,68 @@ class Gamemanger(object):
 
     def register(self, json_data, connid):
         print "register"
+        print json_data
         _ps = pople(json_data["name"], connid,
-                    json_data["x"], json_data["y"], json_data["playerType"])
+                    json_data["x"], json_data["y"], json_data["playerType"], json_data["angle"])
         if _ps.connid in self.clients:
             raise Exception("System record conflict")
         self.clients[_ps.connid] = _ps
         pl, selfplayers = self.getallpopleinfo("init")
         _ps.playerType = 2
-        xx, players = self.getpopleinfobyconnid("init",_ps.connid)
+        xx, players = self.getpopleinfobyconnid("init", _ps.connid)
         return pl, players, selfplayers
 
     def getallpopleinfo(self, command):
         _data = []
         for p in self.clients.values():
             _data.append({"connid": p.connid, 'team': 0, 'tankType': 1, 'playerID': p.connid,
-                          "playerType": p.playerType, "angle":p.angle,"pos": {"x": p.x, "y": p.y}, "name": p.name})
+                          "playerType": p.playerType, "angle": p.angle, "pos": {"x": p.x, "y": p.y}, "name": p.name})
         return self.clients.keys(), {"command": command, "players": _data}
 
     def getpopleinfobyconnid(self, command, connid):
         _data = []
         p = self.clients.get(connid, None)
         _data.append({"connid": p.connid, 'team': 0, 'tankType': 1, 'playerID': p.connid,
-                     "playerType": 2,"angle":p.angle, "pos": {"x": p.x, "y": p.y}, "name": p.name})
+                      "playerType": 2, "angle": p.angle, "pos": {"x": p.x, "y": p.y}, "name": p.name})
         return self.clients.keys(), {"command": command, "players": _data}
 
     def actions(self, json_data):
         p = self.clients.get(json_data['player']['connid'], None)
         _data = {"connid": p.connid, 'team': 0, 'tankType': 1, 'playerID': p.connid,
-                 'playerType': 1, "angle":p.angle,"pos": {"x": p.x, "y": p.y}, "name": p.name}
+                 'playerType': 1, "angle": p.angle, "pos": {"x": p.x, "y": p.y}, "name": p.name}
         return self.clients.keys(), {"command": json_data['command'], "player": _data}
+
+    def _move(self, json_data):
+        p = self.clients.get(json_data["player"]["connid"], None)
+        p.x = json_data.get('player').get("pos").get("x")
+        p.y = json_data.get('player').get("pos").get("y")
+        # return p
+
+    def _rotation(self, json_data):
+        p = self.clients.get(json_data["player"]["connid"], None)
+        p.angle = json_data.get('player').get('angle')
+        # return p
+
+    def switch(self, json_data):
+        return {
+            'move': self._move,
+            'rotation': self._rotation
+        }[json_data["command"]](json_data)
 
     def handledata(self, json_data):
         d = Deferred()
-        try:
-            p = self.clients.get(json_data["player"]["connid"], None)
-            p.x = json_data.get('player').get("pos").get("x")
-            p.y = json_data.get('player').get("pos").get("y")
-            p.angle = json_data.get('player').get('angle')
-        except KeyError as e:
-            print e
-            pass
+        self.switch(json_data)
         d.addCallback(self.actions)
         d.callback(json_data)
         return d
 
 
 class pople(object):
-    def __init__(self, name, connid, x, y, playerType,angle=90):
+    def __init__(self, name, connid, x, y, playerType, angle):
         self.connid = connid
         self.name = name
         self.x = x
         self.y = y
-        self.angle = angle
         self.playerType = playerType
-
+        self.angle = angle
+        
